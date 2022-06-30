29b581d31dfd208d9cf0de4737a171a83cf44f4c
upparvijitha <vijitha031101@gmail.com>
2022-07-01 00:01:54 +0530

    final commit

--- Backend/app.js
+++ Backend/app.js
@@ -2,12 +2,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const router = require("./routes/book-routes");
+const cors = require ("cors");
 const app = express();
 
 
 
 
 app.use(express.json());
+app.use(cors());
 app.use("/books",router)
 
 //middleware
